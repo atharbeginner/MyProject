d1b68d9d15764c6b18845569037bcde190e4ed43
Athar Ali Khan <atharali.khan319@gmail.com>
2023-05-10 04:11:39 +0530

    End of section 6

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
+import { ProjectListComponent } from './projects/project-list/project-list.component';
+import { DonateComponent } from './donate/donate.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AuthGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {
+    path:'',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children:[
+      {path: 'projects',component: ProjectListComponent},
+      {path: 'projects/:id',component: ProjectDetailsComponent},
+      {path: 'donate',component: DonateComponent},
+      {path: 'about-us',component: AboutUsComponent}
+    ]
+  },
+  {path: '**',component: HomeComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,34 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ProjectListComponent } from './projects/project-list/project-list.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
+import { DonateComponent } from './donate/donate.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { SharedModule } from './_module/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    ProjectListComponent,
+    ProjectDetailsComponent,
+    DonateComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
